9ec4448e2bfb20cef6a339ee0b5a78c923d269f3
Craig McKittrick <craig.mckittrick.1@outlook.com>
2025-11-11 08:23:06 -0600

    code is complete!

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ class Program
 
         string? userPlanetChoice = "";
         string planet = "";
+        int deliveryCost = 0;
+        int pizzaA = 0, pizzaB = 0, pizzaC = 0, pizzaD = 0;
 
         // With delivery fee
         var planetOptions = new Dictionary<string, (string Name, int Cost)>
@@ -24,7 +26,7 @@ class Program
         Console.WriteLine($"Hello, {name}! What is your destination planet?");
         foreach (var option in planetOptions)
         {
-            Console.WriteLine($"{option.Key}: {option.Value}");
+            Console.WriteLine($"{option.Key}: {option.Value.Name}");
         }
 
         while (true)
@@ -34,14 +36,15 @@ class Program
             if (planetOptions.ContainsKey(userPlanetChoice))
             {
                 Console.WriteLine();
-                Console.WriteLine($"You have selected {planetOptions[userPlanetChoice]} as your destination. Is that correct? (yes/no)");
+                Console.WriteLine($"You have selected {planetOptions[userPlanetChoice].Name} as your destination. Is that correct? (yes/no)");
 
                 while (true)
                 {
-                    string confirmation = Console.ReadLine()?.ToLower();
+                    string? confirmation = Console.ReadLine()?.ToLower();
                     if (confirmation == "yes")
                     {
                         planet = planetOptions[userPlanetChoice].Name;
+                        deliveryCost = planetOptions[userPlanetChoice].Cost;
                         Console.WriteLine($"Great! Preparing a pizza for delivery to {planet} for {name}.");
                         break;
                     }
@@ -55,13 +58,13 @@ class Program
                         userPlanetChoice = Console.ReadLine()?.ToUpper();
                         if (planetOptions.ContainsKey(userPlanetChoice))
                         {
-                            Console.WriteLine($"You have selected {planetOptions[userPlanetChoice]} as your destination. Is that correct? (yes/no)");
+                            Console.WriteLine($"You have selected {planetOptions[userPlanetChoice].Name} as your destination. Is that correct? (yes/no)");
                             continue;
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Please respond with 'yes' or 'no'.");                        
+                        Console.WriteLine("Please respond with 'yes' or 'no'.");
                     }
                 }
             }
@@ -78,7 +81,7 @@ class Program
         Console.WriteLine("Pizza Type".PadRight(25) + "Cost (credits)");
         Console.WriteLine(new string('-', 45));
 
-        var pizzaTypes = new Dictionary<string, (string Name, int Cost)>
+        var pizzaTypes = new Dictionary<string, (string Name, int pizzaCost)>
             {
                 { "A", ("Galactic Cheese", 10)},
                 { "B", ("Meteor Meat Lover", 15) },
@@ -89,22 +92,38 @@ class Program
 
         foreach (var option in pizzaTypes)
         {
-            Console.WriteLine($"{option.Key}: {option.Value.Name.PadRight(25)} {option.Value.Cost} credits");
+            Console.WriteLine($"{option.Key}: {option.Value.Name.PadRight(25)} {option.Value.pizzaCost} credits");
         }
         Console.WriteLine();
         Console.WriteLine("What type of pizza would you like? Continue to enter requests and type \"done\" when finished.");
-        string? pizzaRequest = "";
-        int totalCost = 0;
+        //string? pizzaRequest = "";
+        int totalPizzaCost = 0;
 
         while (true)
         {
-            pizzaRequest = Console.ReadLine()?.ToUpper();
+            string? pizzaRequest = Console.ReadLine()?.ToUpper();
 
             if (pizzaTypes.ContainsKey(pizzaRequest))
             {
                 Console.WriteLine();
-                totalCost += pizzaTypes[pizzaRequest].Cost;
-                Console.WriteLine($"You have selected {pizzaTypes[pizzaRequest]} as your pizza type. Your total is {totalCost}. Enter another pizza type or type \"done\" to finish.");
+                totalPizzaCost += pizzaTypes[pizzaRequest].pizzaCost;
+                switch (pizzaRequest)
+                {
+                    case "A":
+                        pizzaA++;
+                        break;
+                    case "B":
+                        pizzaB++;
+                        break;
+                    case "C":
+                        pizzaC++;
+                        break;
+                    case "D":
+                        pizzaD++;
+                        break;
+                }
+
+                Console.WriteLine($"You have selected {pizzaTypes[pizzaRequest]} as your pizza type. Your total is {totalPizzaCost}. Enter another pizza type or type \"done\" to finish.");
             }
             else if (pizzaRequest.ToLower() == "done")
             {
@@ -115,8 +134,39 @@ class Program
                 Console.WriteLine("Invalid choice. Please select a valid pizza type (A, B, C, or D).");
             }
         }
+
+        //create summary
         Console.WriteLine();
-        Console.WriteLine($"Thank you, {name}, for your order! Your total cost for delivery to {planet} is {totalCost} credits.");
-        Console.WriteLine("Safe travels and enjoy your Galactic Pizza!");
+        Console.WriteLine($"Thank you, {name}, for your order! See summary below");
+        Console.WriteLine($"Order for {name} to {planet}");
+        Console.WriteLine(new string('-', 30));
+        if (pizzaA > 0)
+        {
+            Console.WriteLine($"{pizzaA} x Galactic Cheese @ 10 = {pizzaA * 10}");
+        }
+        if (pizzaB > 0)
+        {
+            Console.WriteLine($"{pizzaB} x Meteor Meat Lover @ 15 = {pizzaA * 15}");
+        }
+        if (pizzaC > 0)
+        {
+            Console.WriteLine($"{pizzaC} x Veggie Nebula @ 12 = {pizzaA * 12}");
+        }
+        if (pizzaD > 0)
+        {
+            Console.WriteLine($"{pizzaC} x Black Hole BBQ @ 18 = {pizzaA * 18}");
+        }
+        Console.WriteLine($"Subtotal: {totalPizzaCost}");
+        
+        //apply discount
+        double discount = 0;
+        if (pizzaA+pizzaB+pizzaC+pizzaD > 2)
+        {
+            discount = totalPizzaCost * 0.1;
+        }
+        Console.WriteLine($"Discount (10%): {discount}");
+
+        Console.WriteLine($"Delivery Fee: {planetOptions[userPlanetChoice].Cost}");
+        Console.WriteLine($"Total Due: {totalPizzaCost-discount+planetOptions[userPlanetChoice].Cost} credits");
     }
 }
--- obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837afb0521f7d46e0241a1fdb3c2be43a9a8ce86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b96789aaece0907a4dff6ffb31ce2eb70992b11")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
